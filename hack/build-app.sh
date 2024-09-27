8e3ce7f8d7e5a74ba92c53d86459004268d82b7a
elankath <tarun.ramakrishna.elankath@sap.com>
2024-09-27 23:53:10 +0530

    brew SergioBenitez/osxct/x86_64-unknown-linux-gnu and mem inc recorder

--- hack/build-app.sh
+++ hack/build-app.sh
@@ -15,11 +15,23 @@ if [[ -f "$appBinPath" ]]; then
   echo "Removing existing binary."
   rm "$appBinPath"
 fi
+
+if whence -w x86_64-unknown-linux-gnu-gcc > /dev/null 2>&1; then
+    echo "x86_64-unknown-linux-gnu-gcc exists!  Will use for CGO Cross Compilation."
+else
+    echo "x86_64-unknown-linux-gnu-gcc does not exist. Installing via brew..."
+    brew tap SergioBenitez/osxct
+    brew install SergioBenitez/osxct/x86_64-unknown-linux-gnu
+    echo "Installed x86_64-unknown-linux-gnu-gcc -> OPEN FRESH TERMINAL AND RUN THIS SCRIPT: '$0' AGAIN"
+    exit 3
+fi
 echo "Building app for linux/amd64..."
 CC=x86_64-unknown-linux-gnu-gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -o "$appBinPath" cmd/app/main.go
 chmod +x "$appBinPath"
 APP_IMAGE_TAG="$DOCKERHUB_USER/scaling-history-app:latest"
 export APP_IMAGE_TAG
 
-echo "Building and pushing to $APP_IMAGE_TAG..."
+dockerBin=$(which docker)
+echo "Using this docker: $dockerBin"
+echo "Docker building and pushing to $APP_IMAGE_TAG..."
 docker buildx build -f app/Dockerfile --push --platform linux/amd64 --tag "$APP_IMAGE_TAG" .
--- hack/build-recorder.sh
+++ hack/build-recorder.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 set -eo pipefail
 
 echoErr() { echo "$@" 1>&2; }
@@ -10,6 +10,16 @@ fi
 if [[ ! -f cfg/clusters.csv ]]; then
   echoErr "Please ensure that you are in the base dir of the gardener-scaling-history repo before running this script"
   exit 2
+fi
+
+if whence -w  x86_64-unknown-linux-gnu-gcc > /dev/null 2>&1; then
+    echo "x86_64-unknown-linux-gnu-gcc exists!  Will use for CGO Cross Compilation."
+else
+    echo "x86_64-unknown-linux-gnu-gcc does not exist. Installing via brew..."
+    brew tap SergioBenitez/osxct
+    brew install SergioBenitez/osxct/x86_64-unknown-linux-gnu
+    echo "Installed x86_64-unknown-linux-gnu-gcc -> OPEN FRESH TERMINAL AND RUN THIS SCRIPT: '$0' AGAIN"
+    exit 3
 fi
 
 echo "Please ensure that Docker Desktop is started."
@@ -25,5 +35,8 @@ chmod +x bin/recorder
 RECORDER_IMAGE_TAG="$DOCKERHUB_USER/scaling-history-recorder:latest"
 export RECORDER_IMAGE_TAG
 
+echo "Docker building and pushing to $APP_IMAGE_TAG..."
+dockerBin=$(which docker)
+echo "Using this docker: $dockerBin"
 echo "Building and pushing to $RECORDER_IMAGE_TAG..."
 docker buildx build -f recorder/Dockerfile --push --platform linux/amd64 --tag "$RECORDER_IMAGE_TAG" .
